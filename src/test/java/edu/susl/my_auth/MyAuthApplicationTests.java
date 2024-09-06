66f071a311d51926e8c461b02f5f0622ef6b8ca7
asiripr <asiripramodaya2000@gmail.com>
2024-09-06 10:53:32 +0530

    project initialized

---
+++ src/test/java/edu/susl/my_auth/MyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.susl.my_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
